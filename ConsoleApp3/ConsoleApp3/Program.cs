367a714357f9030bb0734cd8224f876266cb3c28
Tkachyk Anna <tkachann17@gmail.com>
2023-05-22 18:10:38 +0300

    sln file commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int count = 40;
+
+            Console.WriteLine("Перші {0} чисел Фібоначчі:", count);
+            for (int i = 0; i < count; i++)
+            {
+                int fibonacciNumber = CalculateFibonacci(i);
+                Console.Write(fibonacciNumber + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int CalculateFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return CalculateFibonacci(n - 1) + CalculateFibonacci(n - 2);
+            }
+        }
+    }
+}
